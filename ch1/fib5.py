afcab13d4cdee7e7940fa8507c4dc165e6a74c09
James Liu <james@pop-os.localdomain>
2019-10-24 22:28:03 -0700

    Oh boy. Will probably just redo git from here

---
+++ ch1/fib5.py
@@ -0,0 +1,15 @@
+def fib5(n: int) -> int:
+    if n == 0:
+        return n
+
+    last: int = 0
+    next: int = 1
+
+    for _ in range(1, n):
+        last, next = next, next + last
+    return next
+
+
+if __name__ == "__main__":
+    print(fib5(5))
+    print(fib5(50))
